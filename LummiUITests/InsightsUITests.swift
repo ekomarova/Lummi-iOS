35f18144b86bd2fe4355c2fae207e028b69c27f0
ekomarova <eva920022@gmail.com>
2026-05-11 03:26:22 +0200

    test: sync UI tests with new InsightsView layout

--- LummiUITests/InsightsUITests.swift
+++ LummiUITests/InsightsUITests.swift
@@ -41,79 +41,40 @@ final class InsightsUITests: XCTestCase {
         XCTAssertTrue(title.waitForExistence(timeout: 2.0), "Insights header did not appear on the screen.")
     }
     
-    // MARK: - Current month state tests
-
-    // Check month state w/o entries
-    func test_CurrentMonth_ZeroState() throws {
+    // Check Joys/Day streak/Joyful hours existence
+    func test_InsightsDefaulEmptyCheckView() throws {
         launchApp(with: [""])
         app.buttons["InsightsButton_Inactive"].tap()
-        
+
         XCTAssertTrue(app.staticTexts["Joys".uppercased()].waitForExistence(timeout: 2.0))
         XCTAssertTrue(app.staticTexts["Day streak".uppercased()].waitForExistence(timeout: 2.0))
-        XCTAssertTrue(app.staticTexts["CompMsg_First"].waitForExistence(timeout: 2.0))
+        XCTAssertTrue(app.staticTexts["Joyful hours".uppercased()].waitForExistence(timeout: 2.0))
     }
     
-    // Check month state with 1 entry
-    func test_CurrentMonth_FewJoys_OneDayStreak() throws {
-        launchApp(with: ["-UI_TESTING_CALENDAR"])
-        app.buttons["InsightsButton_Inactive"].tap()
-        
-        XCTAssertTrue(app.staticTexts["Joys".uppercased()].waitForExistence(timeout: 2.0))
-        XCTAssertTrue(app.staticTexts["Day streak".uppercased()].waitForExistence(timeout: 2.0))
-        XCTAssertTrue(app.staticTexts["CompMsg_Stability"].waitForExistence(timeout: 2.0))
-    }
+    // MARK: - Other tests
     
-    // Check month state with many entry
-    func test_CurrentMonth_ManyJoys_MultipleDaysStreak() throws {
-        launchApp(with: ["-UI_TESTING_CALENDAR", "-UI_TESTING_10_RECORDS"])
-        app.buttons["InsightsButton_Inactive"].tap()
-        
-        XCTAssertTrue(app.staticTexts["Joys".uppercased()].waitForExistence(timeout: 2.0))
-        XCTAssertTrue(app.staticTexts["Day streak".uppercased()].waitForExistence(timeout: 2.0))
-        XCTAssertTrue(app.staticTexts["CompMsg_Growth"].waitForExistence(timeout: 2.0))
-    }
-    
-    // Check month state with fewer entries than past month
-    func test_CurrentMonth_FewerJoysThanPastMonth() throws {
-        launchApp(with: ["-UI_TESTING_CALENDAR", "-UI_TESTING_PAST_MONTH_MANY_JOYS"])
-        app.buttons["InsightsButton_Inactive"].tap()
-        
-        XCTAssertTrue(app.staticTexts["CompMsg_Decline"].waitForExistence(timeout: 2.0))
-    }
-    
-    // MARK: - Past months state tests
-    
-    // Chech month state w/o entries
-    func test_PastMonth_ZeroState() throws {
-        launchApp(with: ["-UI_TESTING_PAST_MONTHS_AVAILABLE"])
-        app.buttons["InsightsButton_Inactive"].tap()
-        app.buttons["PreviousMonthButton"].tap()
-                
-        XCTAssertTrue(app.staticTexts["Joys".uppercased()].waitForExistence(timeout: 2.0))
-        XCTAssertTrue(app.staticTexts["Day streak".uppercased()].waitForExistence(timeout: 2.0))
-        XCTAssertTrue(app.staticTexts["CompMsg_Zero"].waitForExistence(timeout: 2.0))
-    }
-    
-    // Check month state with 1 entry
-    func test_PastMonth_FewJoys_OneDayStreak() throws {
-        launchApp(with: ["-UI_TESTING_PAST_MONTH_ONE_JOY"])
-        app.buttons["InsightsButton_Inactive"].tap()
-        app.buttons["PreviousMonthButton"].tap()
-                
-        XCTAssertTrue(app.staticTexts["Joys".uppercased()].waitForExistence(timeout: 2.0))
-        XCTAssertTrue(app.staticTexts["Day streak".uppercased()].waitForExistence(timeout: 2.0))
-        XCTAssertTrue(app.staticTexts["CompMsg_First"].waitForExistence(timeout: 2.0))
-    }
-    
-    // Check month state with many entry
-    // Borderline case (fails for the first days of the month)
-    func test_PastMonth_ManyJoys_MultipleDaysStreak() throws {
-        launchApp(with: ["-UI_TESTING_PAST_MONTH_MANY_JOYS"])
-        app.buttons["InsightsButton_Inactive"].tap()
-        app.buttons["PreviousMonthButton"].tap()
-                
-        XCTAssertTrue(app.staticTexts["Joys".uppercased()].waitForExistence(timeout: 2.0))
-        XCTAssertTrue(app.staticTexts["Day streak".uppercased()].waitForExistence(timeout: 2.0))
-        XCTAssertTrue(app.staticTexts["CompMsg_First"].waitForExistence(timeout: 2.0))
+    // Check all data on the Insight tab
+    func test_InsightsWithFourDaysFilled() throws {
+        launchApp(with: ["-UI_TESTING_4_DAYS_FILLED"])
+
+        let inactiveInsightsBtn = app.buttons["InsightsButton_Inactive"]
+        XCTAssertTrue(inactiveInsightsBtn.waitForExistence(timeout: 2.0))
+        inactiveInsightsBtn.tap()
+
+        let fours = app.staticTexts.matching(NSPredicate(format: "label == '4'"))
+        XCTAssertTrue(fours.count >= 2, "Expected to find the number '4' for Joys and Day Streak")
+
+        let timePredicate = NSPredicate(format: "label CONTAINS '8:00' AND label CONTAINS '10:00'")
+        let joyfulHoursTime = app.staticTexts.matching(timePredicate).firstMatch
+        XCTAssertTrue(joyfulHoursTime.waitForExistence(timeout: 2.0), "The time of 'Joyful Hours' does not match the expected range")
+
+        let expandButtonText = app.staticTexts["Want to see all moments?".uppercased()]
+        XCTAssertTrue(expandButtonText.waitForExistence(timeout: 2.0))
+        expandButtonText.tap()
+
+        for i in 1...4 {
+            let momentText = app.staticTexts["Evening mock moment \(i)"]
+            XCTAssertTrue(momentText.waitForExistence(timeout: 2.0), "Recording 'Evening mock moment \(i)' did not appear on the list")
+        }
     }
 }
